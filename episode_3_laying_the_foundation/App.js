6bea3df5da868070ac5137efef7ef6609fee951d
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2023-09-13 12:14:18 +0530

    jsx

--- episode_3_laying_the_foundation/App.js
+++ episode_3_laying_the_foundation/App.js
@@ -1,4 +1,8 @@
 
 import React from "react";
 import  ReactDOM  from "react-dom/client";
- 
+ const jsxHeading=<h1>namste react jsx </h1>
+ console.log(jsxHeading);
+
+ const root = ReactDOM.createRoot(document.getElementById("root"));
+ root.render(jsxHeading)
